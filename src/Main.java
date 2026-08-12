abb6eee054809c5985e695a002f3dde58c8457f7
Trần Anh Đài <trananhdai.itqnu@gmail.com>
2026-08-12 15:46:55 +0700

    anhdai

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,5 +7,17 @@ public class Main {
         for(int i = 1; i < 10; i++){
             System.out.print("Xin chào Đài số: " + i + "\n");
         }
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(tinhTong(a,b));
+    }
+
+    public static int tinhTong(int a, int b){
+        return a + b;
     }
 }
+
+
