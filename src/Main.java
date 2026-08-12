8d770451f8dc0db1d11040e1e1239e8b6096fbe2
Trần Anh Đài <trananhdai.itqnu@gmail.com>
2026-08-12 21:19:24 +0700

    dmemooo

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,11 @@ public class Main {
     public static int tinhTong(int a, int b){
         return a + b;
     }
+
+
+    public static int tinhTru(int a, int b){
+        return a - b;
+    }
 }
 
 
